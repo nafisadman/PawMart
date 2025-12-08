90ff0fbca463200546c715b7956f7fe74cc5f93b
Nafi Sadman Khan <sadmannafi789@gmail.com>
2025-12-08 17:14:16 +0600

    A pdf version of my orders can be downloaded via button

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import useTitle from "../hooks/useTitle";
+import { jsPDF } from "jspdf";
 
 const MyOrders = () => {
   useTitle("My Orders");
@@ -19,11 +20,22 @@ const MyOrders = () => {
 
   console.log(myOrders);
 
+  const handleDownloadPDF = () => {
+    const doc = new jsPDF({orientation: "landscape"});
+    // console.log(document.getElementById('my-orders-table').innerText);
+    const htmlTableToText = document.getElementById('my-orders-table').innerText;
+    doc.text(htmlTableToText.toLocaleString(), 10, 50)
+    doc.save("My Orders.pdf");
+  };
+
   return (
     <div>
-      My Orders
+      <div className="flex justify-between">
+        My Orders
+        <button className="btn btn-secondary" onClick={handleDownloadPDF}>Download PDF</button>
+      </div>
       <div className="overflow-x-auto">
-        <table className="table table-xs">
+        <table id="my-orders-table" className="table table-xs">
           <thead>
             <tr>
               <th></th>
@@ -48,12 +60,12 @@ const MyOrders = () => {
                   {myOrder?.date
                     ? new Date(myOrder.date).toLocaleString("en-US", {
                         year: "numeric",
-                        month: "short", 
-                        day: "numeric", 
+                        month: "short",
+                        day: "numeric",
                         hour: "numeric",
                         minute: "numeric",
                         second: "numeric",
-                        hour12: true, 
+                        hour12: true,
                       })
                     : ""}
                 </td>
